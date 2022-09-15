eda3ec6ad862e968afa6a5a7c5a62edb9b782e0e
hmmwhatsthisdo <hmmwhatsthisdo@gmail.com>
2022-09-14 20:30:48 -0700

    Update test format

--- ConvertFrom-Regex.Tests/ConvertFrom-RegEx.Tests.ps1
+++ ConvertFrom-Regex.Tests/ConvertFrom-RegEx.Tests.ps1
@@ -10,14 +10,15 @@ Describe "ConvertFrom-RegEx" {
             @{
                 InputString = "A=1, B=2, C=3"
                 Regex = "A=(?<A>\d+), B=(?<B>\d+), (?:C=(?<C>\d+)[,\s]*)+"
-                Expected = [PSCustomObject]@{
+                ExpectedValue = [PSCustomObject]@{
                     A = "1"
                     B = "2"
-                    C = "3", "4"
+                    C = "3"
                 }
             }
         ) {
-            ($InputString | ConvertFrom-RegEx -Pattern $Regex), $Expected | Test-Equality | Should -BeTrue
+            $ActualValue = $InputString | ConvertFrom-RegEx -Pattern $Regex
+            $ActualValue, $ExpectedValue | Test-Equality | Should -BeTrue -Because "$ActualValue should be deeply equal to $ExpectedValue"
         }
     }
 }

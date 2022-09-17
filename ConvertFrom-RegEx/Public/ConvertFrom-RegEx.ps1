442c408e68c9ab5801dcddcad6f33f9103c5272a
hmmwhatsthisdo <hmmwhatsthisdo@gmail.com>
2022-09-16 20:09:08 -0700

    Add parameter documentation

--- ConvertFrom-RegEx/Public/ConvertFrom-RegEx.ps1
+++ ConvertFrom-RegEx/Public/ConvertFrom-RegEx.ps1
@@ -3,6 +3,15 @@ function ConvertFrom-RegEx {
         DefaultParameterSetName="File"
     )]
     param (
+        <# 
+            Specifies the text to be parsed. Enter a variable that contains the text, or type a command or expression that gets the text.
+
+            Using the InputObject parameter isn't the same as sending strings down the pipeline to `ConvertFrom-RegEx`.
+
+            When you pipe more than one string to the `ConvertFrom-RegEx` cmdlet, it parses each string and returns an object (or objects) for each individual string.
+
+            When you use the InputObject parameter to submit a collection of strings, `ConvertFrom-RegEx` treats the collection as a single combined string. `ConvertFrom-RegEx` returns object(s) if it finds the search text in any component string.
+        #>
         [Parameter(
             ParameterSetName='InputObject', 
             Mandatory,
@@ -13,6 +22,11 @@ function ConvertFrom-RegEx {
         [psobject]
         $InputObject,
         
+        <#
+            Specifies the text to find on each line. The pattern value is treated as a regular expression.
+
+            To learn about regular expressions, see the `about_Regular_Expressions` PowerShell help document.
+        #>
         [Parameter(
             Mandatory,
             Position=0
@@ -20,6 +34,7 @@ function ConvertFrom-RegEx {
         [string[]]
         $Pattern,
 
+        # A path to one or more files to parse. Wildcards are supported.
         [Parameter(
             ParameterSetName='File',
             Mandatory, 
@@ -29,6 +44,7 @@ function ConvertFrom-RegEx {
         [string[]]
         $Path,
 
+        # Specifies the path to the files to be searched. The value of the LiteralPath parameter is used exactly as it's typed. No characters are interpreted as wildcards.
         [Parameter(
             ParameterSetName='LiteralFile', 
             Mandatory, 
@@ -38,11 +54,13 @@ function ConvertFrom-RegEx {
         [string[]]
         $LiteralPath,
     
+        # Indicates that the cmdlet matches should be case-sensitive. By default, matches aren't case-sensitive.
         [Parameter(
         )]
         [switch]
         $CaseSensitive,
 
+        # Indicates that the cmdlet searches for more than one match in each line of text. Without this parameter, `ConvertFrom-RegEx` emits an object for only the first match in each line of text.
         [Parameter(
         )]
         [switch]

3a9fdbc1c1fc47755ba201eabe7f065bc8ba593e
hmmwhatsthisdo <hmmwhatsthisdo@gmail.com>
2022-09-16 21:12:30 -0700

    Add functional tests

--- ConvertFrom-Regex.Tests/Functional.Tests.ps1
+++ ConvertFrom-Regex.Tests/Functional.Tests.ps1
@@ -15,9 +15,49 @@ Describe "ConvertFrom-RegEx" {
                     B = "2"
                     C = "3"
                 }
+                AdditionalParameters = @{}
+            }
+            @{
+                InputString = "A=1, B=2, C=3, C=4"
+                Regex = "A=(?<A>\d+), B=(?<B>\d+), (?:C=(?<C>\d+)[,\s]*)+"
+                ExpectedValue = [PSCustomObject]@{
+                    A = "1"
+                    B = "2"
+                    C = "3", "4"
+                }
+                AdditionalParameters = @{}
+            }
+            @{
+                InputString = "A=1, B=2, C=3, C=4 | A=3, B=4, C=5, C=6"
+                Regex = "A=(?<A>\d+), B=(?<B>\d+), (?:C=(?<C>\d+)[,\s]*)+"
+                ExpectedValue = [PSCustomObject]@{
+                    A = "1"
+                    B = "2"
+                    C = "3", "4"
+                }
+                AdditionalParameters = @{}
+            }
+            @{
+                InputString = "A=1, B=2, C=3, C=4 | A=3, B=4, C=5, C=6"
+                Regex = "A=(?<A>\d+), B=(?<B>\d+), (?:C=(?<C>\d+)[,\s]*)+"
+                ExpectedValue = @(
+                    [PSCustomObject]@{
+                        A = "1"
+                        B = "2"
+                        C = "3", "4"
+                    },
+                    [PSCustomObject]@{
+                        A = "3"
+                        B = "4"
+                        C = "5", "6"
+                    }
+                )
+                AdditionalParameters = @{
+                    AllMatches = $true
+                }
             }
         ) {
-            $ActualValue = $InputString | ConvertFrom-RegEx -Pattern $Regex
+            $ActualValue = $InputString | ConvertFrom-RegEx -Pattern $Regex @AdditionalParameters
             Assert-Equivalent -Actual $ActualValue -Expected $ExpectedValue -StrictOrder
         }
     }
